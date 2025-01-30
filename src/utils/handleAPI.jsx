d04b388c23b454dc16b4b024ff6c1da467fd1c8b
Fon Ronard <127010963+LordRona@users.noreply.github.com>
2025-01-30 04:46:27 +0100

    Add files via upload

---
+++ src/utils/handleAPI.jsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const baseurl = 'http://localhost:5000';
+
+const getAllToDo = (setToDo)=> {
+    axios
+    .get(baseurl)
+    .then(({data})=> {
+        console.log("Data -----> ", data);
+        setToDo(data);
+    })
+}
+
+const addToDo = (text, setText, setToDo)=> {
+    axios
+    .post(`${baseurl}/save`, {text})
+    .then((data)=> {
+        console.log(data);
+        setText("")
+        getAllToDo(setToDo)
+    }).catch((err)=> console.log("Error occured", err)
+    )
+}
+
+const updateToDo = (toDoId, text, setText, setToDo, setIsUpdating)=> {
+    axios
+    .post(`${baseurl}/update`, {_id: toDoId, text})
+    .then((data)=> {
+        setText("")
+        setIsUpdating(false);
+        getAllToDo(setToDo);
+    }).catch((err)=> console.log("Error occured", err)
+)
+}
+
+const deleteToDo = (_id, setToDo)=> {
+    axios
+    .post(`${baseurl}/delete`, {_id })
+    .then((data)=> {
+        getAllToDo(setToDo);
+    }).catch((err)=> console.log("Error occured", err)
+)
+}
+
+export {getAllToDo, addToDo, updateToDo, deleteToDo};
